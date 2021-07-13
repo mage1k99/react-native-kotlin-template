766c6f88869f88bb8d2382b35f2a130d6d1ef835
MageshBabu K <mage1k99@fossfreaks.com>
2021-07-13 22:16:49 +0530

    inital commit

---
+++ template/android/app/src/main/java/com/fossfreaks/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.fossfreaks
+
+import com.facebook.react.ReactActivity
+
+class MainActivity : ReactActivity() {
+
+    override fun getMainComponentName(): String? {
+        return "projectName" // Replace templatee with your Project Name in app.json
+    }
+}
