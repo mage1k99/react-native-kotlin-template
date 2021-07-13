0c1dc9b96c199f3bda9f97edf96d12b1e0392666
MageshBabu K <mage1k99@fossfreaks.com>
2021-07-13 22:39:53 +0530

    migrating to SwiftUI
    No Idea if this will work or not

---
+++ template/ios/ProjectName/AppDelegate.swift
@@ -0,0 +1,20 @@
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  var bridge: RCTBridge!
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    let jsCodeLocation: URL
+
+    jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackResource:nil)
+    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "ProjectName", initialProperties: nil, launchOptions: launchOptions)
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = rootViewController
+    self.window?.makeKeyAndVisible()
+
+    return true
+  }
+}
